7b6966af249957d5270cc388aabec76c9e8888f5
Mitchell Walker <mwalker5@uccs.edu>
2018-10-03 02:38:42 +0000

    edited routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   get 'startup/index'
+  get 'startup/index/user'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
