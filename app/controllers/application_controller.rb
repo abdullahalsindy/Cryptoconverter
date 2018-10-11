a9a9fdd04b6cbc417356d128b17b91e5af860723
Mitchell Walker <mwalker5@uccs.edu>
2018-10-11 18:22:50 +0000

    Style changes before demo

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,5 +3,5 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   #add this when calculator is done to make sure only our users can see it
-    #before_action :authenticate_user!, except: [:index, :application]
+    #before_action :authenticate_user!, except: [:index, :application] 
 end
--- app/controllers/coins_controller.rb
+++ app/controllers/coins_controller.rb
@@ -1,16 +1,16 @@
 require_dependency 'crypto_compare.rb' 
 
 class CoinsController < ApplicationController
-
+before_filter :authenticate_user!
   
   def index
     @coin = Coin.ratings
     @calculator = CryptoCompare.new
     @input = params[:input] || 'BTC'
     @output = params[:output] || 'USD'
-    @q = params[:qty].to_i || 1
+    if params[:qty].to_i <= 0 then @q = 1 else @q = params[:qty].to_i end
     @exchange = @calculator.request(@input, @output)
-    @exchange = @exchange.nil? ? "No data available please report to the site owners": "#{@q} (#{@input})" +"=" + "#{ @q * @exchange} (#{@output})"
+    @exchange = @exchange.nil? ? "No data available please report to the site owners": "#{@q} (#{@input})" +" = " + "#{ (@q * @exchange)} (#{@output})"
 
   end
   
