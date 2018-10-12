0945770453bd1d658e5008e0fe9724ddf5a72c93
Galen Eppler <geppler@uccs.edu>
2018-10-12 17:50:29 -0600

    First step definition for currency calculation passes

--- features/step_definitions/curr_calc_steps.rb
+++ features/step_definitions/curr_calc_steps.rb
@@ -5,6 +5,9 @@ Given("I am on the trade page") do
 end
 
 When("i click the exchange button") do
-    click_on "Convert"
+    	click_on "Convert"
 end
 
+Then("i should be given the exchange rate between the coins") do
+	:exchange == :calculator#calculator(:input, :output)
+end
