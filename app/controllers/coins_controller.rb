f77bd872125885d3169162c6173c36dc89b91491
Mitchell Walker <mwalker5@uccs.edu>
2018-10-24 00:25:45 +0000

    changed style related to graph

--- app/controllers/coins_controller.rb
+++ app/controllers/coins_controller.rb
@@ -13,11 +13,12 @@ before_filter :authenticate_user!
     if params[:qty].to_i <= 0 then @q = 1 else @q = params[:qty].to_i end
     @exchange = @calculator.request(@input, @output)
     @graphData = @calculator.history_to_day(@input, @output, 30)
+    #@graphDataUSD = @calculator.history_to_day(@input, 'USD', 30)
     
-    
-    
-    @dates = @graphData.map {|x| Time.at(x.values[0]).to_date.day}
-    @prices = @graphData.map {|x| x.values[1]}
+    #@dataUSD = @graphDataUSD.map {|x| [x.values[0]*1000.0,x.values[1]]}
+    @data = @graphData.map {|x| [x.values[0]*1000.0,x.values[1]]}
+    #@dates = @graphData.map {|x| Time.at(x.values[0]).to_date.strftime('%v')}
+    #@prices = @graphData.map {|x| x.values[1]}
      
     @exchange = @exchange.nil? ? "No data available please report to the site owners": "#{@q} (#{@input})" +" = " + "#{ (@q * @exchange)} (#{@output})"
 
