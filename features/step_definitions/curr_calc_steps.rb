432afe3b827600f03bbad5d45eb7a781fff04d82
Galen Eppler <geppler@uccs.edu>
2018-10-28 08:45:02 -0600

    Fixed a typo

--- features/step_definitions/curr_calc_steps.rb
+++ features/step_definitions/curr_calc_steps.rb
@@ -21,30 +21,30 @@ When("I click on the assets button [1]") do
     click_on "Assets"
 end
 Then("I should be given the choice to select a type of currency") do
-    @input = params[:input]
+     @input = params[:input]
 end
 
-Given("I am on the trade page [2]")
+Given("I am on the trade page [2]") do
     if (:user)
         visit root_path
     end
 end
-When("I click on the assets button [2]")
+When("I click on the assets button [2]") do
     click_on "Assets"
 end
-Then("I should be given the choice to select the amount of my selected currency")
-    @amount = :amount
+Then("I should be given the choice to select the amount of my selected currency") do
+     @amount = :amount
 end
 
-Given("I am on the trade page")
+Given("I am on the trade page [3]") do
     if (:user)
         visit root_path
     end
 end
-When("I submit my selected assets and amount")
+When("I submit my selected assets and amount") do
     click_on "Submit"
 end
-Then("I should see those assets reflected in the database")
-    @params[:input] = @input
-    @params[:amount] = @amount
+Then("I should see those assets reflected in the database") do
+     @params[:input] = @input
+     @params[:amount] = @amount
 end
