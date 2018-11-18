0df67900f779edb51d928b19bceed7dc6a76b9c2
abdullaha360 <abdullah.a360@gmail.com>
2018-11-18 02:01:42 +0000

    added favicon

--- app/controllers/coins_controller.rb
+++ app/controllers/coins_controller.rb
@@ -14,15 +14,8 @@ before_filter :authenticate_user!
     if params[:qty].to_i <= 0 then @q = 1 else @q = params[:qty].to_i end
     @exchange = @calculator.request(@input, @output)
     @graphData = @calculator.history_to_day(@input, @output, 30)
-    #@graphDataUSD = @calculator.history_to_day(@input, 'USD', 30)
-    
-    #@dataUSD = @graphDataUSD.map {|x| [x.values[0]*1000.0,x.values[1]]}
     @data = @graphData.map {|x| [x.values[0]*1000.0,x.values[1]]}
-    #@dates = @graphData.map {|x| Time.at(x.values[0]).to_date.strftime('%v')}
-    #@prices = @graphData.map {|x| x.values[1]}
-     
     @exchange = @exchange.nil? ? "No data available please report to the site owners": "#{@q} (#{@input})" +" = " + "#{ (@q * @exchange)} (#{@output})"
-
-  end
+    end
   
 end
